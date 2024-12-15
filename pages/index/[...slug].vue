b2d8ad388824e62a8d70bf54e766e9b0986ff139
Imna <gio.imnaishvili029@gmail.com>
2024-12-15 04:46:38 -0500

    add back and forward buttons

--- pages/index/[...slug].vue
+++ pages/index/[...slug].vue
@@ -28,18 +28,31 @@ function navigateToPathFromMenu(path: string) {
 
 <template>
   <div class="flex flex-col h-full min-h-0">
-    <div class=" flex-shrink-0">
-      <Breadcrumb :model="menuItems">
-        <template #item="{item}">
-          <div v-if="item.label!==''" class="cursor-pointer" @click="navigateToPathFromMenu(item.label as string)">
-            {{ item.label }}
-          </div>
-          <div v-else>
-            <Icon name="solar:home-outline" class="cursor-pointer" @click="navigateToPathFromMenu('')"
-            ></Icon>
-          </div>
-        </template>
-      </Breadcrumb>
+    <div class="flex-shrink-0">
+      <div class="flex flex-row items-center w-full gap-2 bg-surface-900 h-full">
+        <div class="flex flex-row">
+          <button class="hover:bg-surface-400 rounded-lg flex flex-row items-center h-full" @click="useRouter().back()">
+            <Icon name="solar:arrow-left-line-duotone" size="2em" mode="css" class=""/>
+          </button>
+          <button class=" hover:bg-surface-400 rounded-lg flex flex-row items-center  h-full"
+                  @click="useRouter().forward()">
+            <Icon name="solar:arrow-right-line-duotone" size="2em" mode="css" class=""/>
+          </button>
+        </div>
+        <div class="w-full">
+          <Breadcrumb :model="menuItems">
+            <template #item="{item}">
+              <div v-if="item.label!==''" class="cursor-pointer" @click="navigateToPathFromMenu(item.label as string)">
+                {{ item.label }}
+              </div>
+              <div v-else>
+                <Icon name="solar:home-outline" class="cursor-pointer" @click="navigateToPathFromMenu('')"
+                ></Icon>
+              </div>
+            </template>
+          </Breadcrumb>
+        </div>
+      </div>
     </div>
     <div class="flex w-dvw flex-1 h-full min-h-0">
       <div class="h-full bg-surface-900 max-w-64 col-span-2 w-32 flex-1">
